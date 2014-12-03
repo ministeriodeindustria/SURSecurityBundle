5e8a104a6f1e2251e3b6d114c85ca47a99b72e29
kevin <kevin1992@gmail.com>
2014-12-03 11:14:50 -0300

    Agregado access inscripcion

--- Security/Helper/InscripcionAccessStrategy.php
+++ Security/Helper/InscripcionAccessStrategy.php
@@ -3,8 +3,8 @@ namespace SUR\SecurityBundle\Security\Helper;
 
 class InscripcionAccessStrategy implements IAccessStrategy {
 
-  public function hasAccess($role) {
-    return $role=='_inscripcion';
-  }
-  
+	public function hasAccess($role) {
+		return $role=='_inscripcion';
+	}
+
 }
